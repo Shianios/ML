ad99c6cf353b3554cc65619c92dbd14233fecd3c
Shianios <45406246+Shianios@users.noreply.github.com>
2019-02-28 20:55:28 +0200

    Add files via upload

--- act_funcs.py
+++ act_funcs.py
@@ -9,121 +9,115 @@ import numpy as np
 
 class act_funcs:
     def __init__(self,data,**kwargs):
+        if 'func' not in kwargs: kwargs['func'] = 'sigmoid'
         self.kwargs = kwargs
         self.data = data
     
     def compute(self):
         return getattr(self, self.kwargs.get('func'))(self.data,**self.kwargs)
+
+    def param_extract_contineus(func):  # For the contineus act funcs parameters have the same form.
+        def extract(*args,**kwargs):           
+            if 'order' not in kwargs:
+                kwargs['order'] = 0
+            if 'params' in kwargs:
+                params = kwargs['params']
+                del kwargs['params']
+            try:
+                params
+            except NameError:
+                kwargs['s'] = 1.
+                kwargs['a'] = 1.
+                kwargs['m'] = 0.
+            else: # Maybe one or two of the parameters is defined in the params, but not all.
+                if 's' in params: kwargs['s'] = params.get('s')
+                else: kwargs['s'] = 1.
+                if 'm' in params: kwargs['m'] = params.get('m')
+                else: kwargs['m'] = 0.
+                if 'a' in params: kwargs['a'] = params.get('a')
+                else: kwargs['a'] = 1.
+            result = func(*args,**kwargs)
+            return result      
+        return extract
     
-    # Functions meathods
-    def sigmoid (self,data,**kwargs):  
-        order = int(kwargs['order'])
-        params = kwargs['params']
-        if not params:
-            s = a = 1.
-            m = 0.
-        else:
-            if 's' in params: s = params.get('s')
-            else: s = 1.
-            if 'm' in params: m = params.get('m')
-            else: m = 0.
-            if 'a' in params: a = params.get('a')
-            else: a = 1.
-        
-        if order == 0:
-            Exp = np.exp(np.add(m,np.multiply(-a,data)))
-            data = np.divide(s,np.add(1.,Exp))
+    @staticmethod
+    @param_extract_contineus
+    def sigmoid(data,**kwargs):
+        if kwargs['order'] == 0:
+            Exp = np.exp(np.add(kwargs['m'],np.multiply(-1.*kwargs['a'],data)))
+            data = np.divide(kwargs['s'],np.add(1.,Exp))
             del Exp
-        elif order == 1:
-            Exp = np.exp(np.add(m,np.multiply(-a,data)))
+        elif kwargs['order'] == 1:
+            Exp = np.exp(np.add(kwargs['m'],np.multiply(-kwargs['a'],data)))
             Exp1 = np.add(1.,Exp)
-            data = np.divide(np.multiply(a*s,Exp),np.multiply(Exp1,Exp1))
+            data = np.divide(np.multiply(kwargs['a']*kwargs['s'],Exp),np.multiply(Exp1,Exp1))
             del Exp, Exp1
         else:
             print('not implimented yet')
-        del s,m,a
-        return data
-            
-    def tanh (self,data,**kwargs):
-        order = int(kwargs['order'])
-        params = kwargs['params']
-        if not params:
-            s = a = 1.
-            m = 0.
-        else:
-            if 's' in params: s = params.get('s')
-            else: s = 1.
-            if 'm' in params: m = params.get('m')
-            else: m = 0.
-            if 'a' in params: a = params.get('a')
-            else: a = 1.
-            
-        if order == 0:
-            d = np.add(m,np.multiply(a,data))
-            data = np.multiply(s,np.tanh(d))
-            del d
-        elif order == 1:
-            d = np.add(m,np.multiply(a,data))
-            tanh = np.tanh(d)
-            data = np.multiply(s,np.subtract(1.,np.multiply(tanh,tanh)))
-            del d, tanh
-        del s,m,a
-        return data
-            
-    def gauss (self,data,**kwargs):
-        order = int(kwargs['order'])
-        params = kwargs['params']
-        if not params:
-            s = a = 1.
-            m = 0.
-        else:
-            if 's' in params: s = params.get('s')
-            else: s = 1.
-            if 'm' in params: m = params.get('m')
-            else: m = 0.
-            if 'a' in params: a = params.get('a')
-            else: a = 1.
-        
-        if order == 0:
-            d = np.subtract(data,m)
-            d = np.multiply(d,d)
-            d = np.divide(d,-a)
-            data = np.multiply(s,np.exp(d))
-            del d
-        elif order == 1:
-            d = np.subtract(data,m)
-            Exp = np.exp(np.divide(np.multiply(d,d),-a))
-            data = np.divide(np.multiply(-2*a*s,np.multiply(Exp,d)),a)
-            del d, Exp
-        del s,m,a
-        return data
-            
-    def inv_gauss (self,data,**kwargs):
-        order = int(kwargs['order'])
-        params = kwargs['params']
-        if not params:
-            s = a = 1.
-            m = 0.
-        else:
-            if 's' in params: s = params.get('s')
-            else: s = 1.
-            if 'm' in params: m = params.get('m')
-            else: m = 0.
-            if 'a' in params: a = params.get('a')
-            else: a = 1.
-            
-        if order == 0:
-            d = np.subtract(data,m)
-            d = np.multiply(d,d)
-            d = np.divide(d,-a)
-            data = np.multiply(s,np.subtract(1.,np.exp(d)))
-            del d
-        elif order == 1:
-            d = np.subtract(data,m)
-            Exp = np.exp(np.divide(np.multiply(d,d),-a))
-            data = np.divide(np.multiply(2*a*s,np.multiply(Exp,d)),a)
-            del d, Exp
-        del s,m,a
         return data
     
+    @staticmethod
+    @param_extract_contineus
+    def tanh (data,**kwargs):         
+        if kwargs['order'] == 0:
+            d = np.add(kwargs['m'],np.multiply(kwargs['a'],data))
+            data = np.multiply(kwargs['s'],np.tanh(d))
+            del d
+        elif kwargs['order'] == 1:
+            d = np.add(kwargs['m'],np.multiply(kwargs['a'],data))
+            tanh = np.tanh(d)
+            data = np.multiply(kwargs['s'],np.subtract(1.,np.multiply(tanh,tanh)))
+            del d, tanh
+        return data
+    
+    @staticmethod
+    @param_extract_contineus        
+    def gauss (data,**kwargs):
+        if kwargs['order'] == 0:
+            d = np.subtract(data,kwargs['m'])
+            d = np.multiply(d,d)
+            d = np.divide(d,-1.*kwargs['a'])
+            data = np.multiply(kwargs['s'],np.exp(d))
+            del d
+        elif kwargs['order'] == 1:
+            d = np.subtract(data,kwargs['m'])
+            Exp = np.exp(np.divide(np.multiply(d,d),-1.*kwargs['a']))
+            data = np.divide(np.multiply(-2.*kwargs['a']*kwargs['s'],np.multiply(Exp,d)),kwargs['a'])
+            del d, Exp
+        return data
+    
+    @staticmethod
+    @param_extract_contineus        
+    def inv_gauss (data,**kwargs):          
+        if kwargs['order'] == 0:
+            d = np.subtract(data,kwargs['m'])
+            d = np.multiply(d,d)
+            d = np.divide(d,-1.*kwargs['a'])
+            data = np.multiply(kwargs['s'],np.subtract(1.,np.exp(d)))
+            del d
+        elif kwargs['order'] == 1:
+            d = np.subtract(data,kwargs['m'])
+            Exp = np.exp(np.divide(np.multiply(d,d),-1.*kwargs['a']))
+            data = np.divide(np.multiply(2.*kwargs['a']*kwargs['s'],np.multiply(Exp,d)),kwargs['a'])
+            del d, Exp
+        return data
+    
+'''
+# Test
+d = np.arange(5)
+res1 = act_funcs.sigmoid(d)
+res2 = act_funcs(d,**{'func':'sigmoid'}).compute()
+res3 = act_funcs(d).compute()
+
+Exp = np.exp(np.add(0.,np.multiply(-1.*1.,d)))
+res4 = np.divide(1.,np.add(1.,Exp))
+
+print(res1)
+print(res2)
+print(res3)
+print(res4)
+'''
+
+
+
 
